9fb17a9ac48f1587e134e01102a6dcd4c0a28222
ashutosh birthare <86846323+ashuashu2@users.noreply.github.com>
2023-12-21 17:47:57 +0530

    update

--- routes/restaurant.route.js
+++ routes/restaurant.route.js
@@ -474,9 +474,13 @@ async function addRatingAndReview(restaurantId, userId, rating, reviewText) {
       restaurant.reviews.push(review);
 
       await restaurant.save();
+      console.log(reviewText)
+      console.log(restaurantId)
+
       
 
       const updatedRestaurantWithReview = await restaurantModel.findById(restaurantId)
+      console.log(updateRestaurantDetails)
       return updatedRestaurantWithReview;
     } 
   } catch (error) {
